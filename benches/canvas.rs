a032f34e4c15bd6c8091701b8abb30bcab027e07
kolen <incredible.angst@gmail.com>
2019-04-12 15:52:06 +0300

    Benchmark on single instance of Canvas (more robust this way)

--- benches/canvas.rs
+++ benches/canvas.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate criterion;
 
+use criterion::black_box;
 use criterion::Criterion;
-use criterion::{black_box, BatchSize};
 
 extern crate rustzx;
 use rustzx::utils::clocks::Clocks;
@@ -11,29 +11,23 @@ use rustzx::zx::screen::canvas::ZXCanvas;
 
 fn process_clocks_benchmark(c: &mut Criterion) {
     c.bench_function("Canvas::process_clocks", |b| {
-        b.iter_batched(
-            || ZXCanvas::new(ZXMachine::Sinclair48K),
-            |mut canvas: ZXCanvas| {
-                for f in 0..ZXMachine::Sinclair48K.specs().clocks_frame {
-                    black_box(canvas.process_clocks(Clocks(f)));
-                }
-            },
-            BatchSize::SmallInput,
-        )
+        let mut canvas = ZXCanvas::new(ZXMachine::Sinclair48K);
+        b.iter(|| {
+            for f in 0..ZXMachine::Sinclair48K.specs().clocks_frame {
+                black_box(canvas.process_clocks(Clocks(f)));
+            }
+        });
     });
 }
 
 fn update_benchmark(c: &mut Criterion) {
     c.bench_function("Canvas::update", |b| {
-        b.iter_batched(
-            || ZXCanvas::new(ZXMachine::Sinclair48K),
-            |mut canvas: ZXCanvas| {
-                for offset in 0..0x1b00 {
-                    black_box(canvas.update(offset, 0, 0b1010_1010));
-                }
-            },
-            BatchSize::SmallInput,
-        )
+        let mut canvas = ZXCanvas::new(ZXMachine::Sinclair48K);
+        b.iter(|| {
+            for offset in 0..0x1b00 {
+                black_box(canvas.update(offset, 0, 0b1010_1010));
+            }
+        })
     });
 }
 
